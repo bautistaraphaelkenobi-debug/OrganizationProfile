5ccc2456c16f7594172b9ad693117517590e3f67
bautistaraphaelkenobi-debug <bautista.raphaelkenobi@gmail.com>
2025-09-05 23:51:15 +0800

    Add files via upload

---
+++ OrganizationProfile/AccountRegistration/frmConfirmation.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace AccountRegistration
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStudentNo = new System.Windows.Forms.Label();
+            this.lblProgram = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lblContactNo = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.lblBirthday = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(45, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Number:";
+            // 
+            // lblStudentNo
+            // 
+            this.lblStudentNo.AutoSize = true;
+            this.lblStudentNo.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.lblStudentNo.Location = new System.Drawing.Point(226, 38);
+            this.lblStudentNo.Name = "lblStudentNo";
+            this.lblStudentNo.Size = new System.Drawing.Size(0, 16);
+            this.lblStudentNo.TabIndex = 1;
+            // 
+            // lblProgram
+            // 
+            this.lblProgram.AutoSize = true;
+            this.lblProgram.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.lblProgram.Location = new System.Drawing.Point(226, 76);
+            this.lblProgram.Name = "lblProgram";
+            this.lblProgram.Size = new System.Drawing.Size(0, 16);
+            this.lblProgram.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(45, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Program:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.lblName.Location = new System.Drawing.Point(226, 113);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(0, 16);
+            this.lblName.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(45, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Name:";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.lblAge.Location = new System.Drawing.Point(226, 151);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(0, 16);
+            this.lblAge.TabIndex = 11;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.label12.Location = new System.Drawing.Point(45, 151);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(37, 16);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "Age:";
+            // 
+            // lblContactNo
+            // 
+            this.lblContactNo.AutoSize = true;
+            this.lblContactNo.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.lblContactNo.Location = new System.Drawing.Point(226, 259);
+            this.lblContactNo.Name = "lblContactNo";
+            this.lblContactNo.Size = new System.Drawing.Size(0, 16);
+            this.lblContactNo.TabIndex = 13;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.label14.Location = new System.Drawing.Point(45, 259);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(120, 16);
+            this.label14.TabIndex = 12;
+            this.label14.Text = "Contact Number:";
+            // 
+            // lblBirthday
+            // 
+            this.lblBirthday.AutoSize = true;
+            this.lblBirthday.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.lblBirthday.Location = new System.Drawing.Point(226, 189);
+            this.lblBirthday.Name = "lblBirthday";
+            this.lblBirthday.Size = new System.Drawing.Size(0, 16);
+            this.lblBirthday.TabIndex = 15;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.label16.Location = new System.Drawing.Point(45, 189);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(67, 16);
+            this.label16.TabIndex = 14;
+            this.label16.Text = "Birthday:";
+            this.label16.Click += new System.EventHandler(this.label16_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(132, 311);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(86, 28);
+            this.btnSubmit.TabIndex = 16;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.lblGender.Location = new System.Drawing.Point(226, 224);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(0, 16);
+            this.lblGender.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(45, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Gender:";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(434, 376);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblBirthday);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.lblContactNo);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblProgram);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblStudentNo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "frmConfirmation";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStudentNo;
+        private System.Windows.Forms.Label lblProgram;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lblContactNo;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label lblBirthday;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ OrganizationProfile/AccountRegistration/frmConfirmation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AccountRegistration
+{
+    public partial class Form2 : Form
+    {                  
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+       
+        public Form2()
+        {
+            InitializeComponent();
+
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+            lblStudentNo.Text = StudentInformationClass.SetStudentNo.ToString();
+            lblName.Text = StudentInformationClass.SetFullName;
+            lblProgram.Text = StudentInformationClass.SetProgram;
+            lblBirthday.Text = StudentInformationClass.SetBirthDay;
+            lblGender.Text = StudentInformationClass.SetGender;
+            lblContactNo.Text = StudentInformationClass.SetContactNo.ToString();
+            lblAge.Text = StudentInformationClass.SetAge.ToString();
+
+        }
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+         
+            string connectionString = "Server=DESKTOP-PETL4CO\\SQLEXPRESS;Database=NINE;Trusted_Connection=True;";
+
+          
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = @"INSERT INTO StudentInformation 
+                         (StudentNo, FullName, Program, BirthDay, Gender, ContactNo, Age) 
+                         VALUES 
+                         (@StudentNo, @FullName, @Program, @BirthDay, @Gender, @ContactNo, @Age)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@StudentNo", StudentInformationClass.SetStudentNo);
+                cmd.Parameters.AddWithValue("@FullName", StudentInformationClass.SetFullName);
+                cmd.Parameters.AddWithValue("@Program", StudentInformationClass.SetProgram);
+                cmd.Parameters.AddWithValue("@BirthDay", StudentInformationClass.SetBirthDay);
+                cmd.Parameters.AddWithValue("@Gender", StudentInformationClass.SetGender);
+                cmd.Parameters.AddWithValue("@ContactNo", StudentInformationClass.SetContactNo);
+                cmd.Parameters.AddWithValue("@Age", StudentInformationClass.SetAge);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Registration saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Database error: " + ex.Message, "SQL Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+           
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
